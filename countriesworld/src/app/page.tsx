02698ce1f0d8d8ac77429456a80e2b6e26c57bf6
Jederson Andre <andre_sjx@live.com>
2024-07-03 11:46:31 -0300

    :sparkles: feat: Desafio Codante.io

---
+++ countriesworld/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <h1>Countries Of World</h1>
+    </main>
+  );
+}
